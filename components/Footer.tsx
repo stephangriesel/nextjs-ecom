87ad39c509eb6fc075088c3c1869b37473b5a9d2
Stephan <sgriesel@gmail.com>
2021-08-05 06:41:22 +0200

    add header footer to layout (wip)

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(){
+    return (
+        <footer>
+            <p>
+                Woof! &nbsp;<a href="<https://sleepydoggy.shop>">&nbsp; 🐕</a>
+            </p>
+        </footer>
+    )
+}
